d7712e1e4cea8464c215139787d5a2bf34ef1973
Menno van Grinsven <scanno71@gmail.com>
2018-06-25 07:02:15 +0200

    webview: use Monochrome build for webview and chromium
    Change-Id: I8e381cb558d40ac4183e245321522df481d80356

--- Android.mk
+++ Android.mk
@@ -16,25 +16,29 @@
 
 # Install the prebuilt webview apk.
 
-LOCAL_PATH := $(call my-dir)
+ifneq ($(TARGET_USES_AOSP_BROWSER),true)
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := webview
+LOCAL_MODULE       := Chromium
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := APPS
+#LOCAL_SRC_FILES    := 64bit/MonochromePublic.apk
+#LOCAL_CERTIFICATE  := PRESIGNED
+LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
 LOCAL_MULTILIB := both
 LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
 LOCAL_REQUIRED_MODULES := \
-        libwebviewchromium_loader \
-        libwebviewchromium_plat_support
+        libmonochrome_loader \
+        libmonochrome_plat_support
 
-LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
+LOCAL_MODULE_TARGET_ARCH := arm arm64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/webview.apk
+LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/MonochromePublic.apk
 
-LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
-LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
-LOCAL_PREBUILT_JNI_LIBS_x86 := @lib/x86/libwebviewchromium.so
-LOCAL_PREBUILT_JNI_LIBS_x86_64 := @lib/x86_64/libwebviewchromium.so
+LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libmonochrome.so
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libmonochrome.so
 
 include $(BUILD_PREBUILT)
+endif
+
